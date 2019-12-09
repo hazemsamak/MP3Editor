06613d98f794bcae0d613260b085b743ffd0b607
Hazem Samak <hsamak@cisco.com>
2019-12-09 14:31:40 +0400

    Fix the path

--- MP3Manager.py
+++ MP3Manager.py
@@ -1,9 +1,11 @@
 from MP3 import tagMP3Files
 from Rename import renameFolderFiles
+import os
 
 def main():
     path = input("Enter the directory path: ")
-    renameFolderFiles(path)
+    absolute_path = os.path.abspath(path)
+    renameFolderFiles(absolute_path)
     tagMP3Files(path)
 
 if __name__ == "__main__":
